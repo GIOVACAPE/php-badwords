cdcd503ce3d83e48eacb419dd2e31b3d310e6270
Giovanni Capelli <giovacape01@gmail.com>
2023-02-21 11:18:07 +0100

    completato

--- index.php
+++ index.php
@@ -21,6 +21,37 @@
     <title>Bad Words GC</title>
 </head>
 <body>
-    
+    <h1>Via del campo</h1>
+    <h3>Fabrizio De Andrè</h3>
+
+    <p>
+        <?php echo $canzone; ?>
+    </p>
+    <h6>Questa canzone ha
+     <?php echo strlen($canzone); ?>
+     caratteri.
+    </h6>
+
+    <!-- LATO UTENTE -->
+    <form action="" method="GET">
+        <input type="text" name="censura" placeholder="Inserisci la parola che vorresti censurare.." size="50">
+        <button>
+            Invio
+        </button>
+    </form>
+
+    <h4>La canzone censurata:</h4>
+    <p>
+        <?php echo $canzoneCensurata= str_replace("$parolaCensurata", "***", $canzone) ?>
+    </p>
+    <h6>
+        la canzone censurata è lunga
+        <?php echo strlen($canzoneCensurata); ?>
+        caratteri.
+    </h6>
+
+
 </body>
 </html>
+
+<!-- <?php ?> -->
