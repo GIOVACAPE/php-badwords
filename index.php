d5f990a3539e26648df17f44cc759620c4465986
Giovanni Capelli <giovacape01@gmail.com>
2023-02-21 10:43:57 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+    $canzone = "Via del Campo, c'è una puttana
+
+    Gli occhi grandi color di foglia
+
+    Se di amarla ti vien la voglia
+
+    Basta prenderla per la mano" ;
+
+    $parolaCensurata = $_GET["censura"];
+    
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Words GC</title>
+</head>
+<body>
+    
+</body>
+</html>
